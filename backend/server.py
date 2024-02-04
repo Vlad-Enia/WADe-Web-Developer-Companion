15699b95eaf532fcdf8a66a86fee0d4184380ce6
Vlad-Enia <58520544+Vlad-Enia@users.noreply.github.com>
2024-02-04 23:18:55 +0200

    Reformat backend

--- backend/server.py
+++ backend/server.py
@@ -1,13 +1,7 @@
-from flask import Flask, request, make_response, jsonify, abort
+from flask import Flask, request, make_response
 from hashlib import sha256
-from login.login_service import _build_cors_preflight_response, _corsify_actual_response, encode_auth_token, decode_auth_token, authenticate, is_authorized
-import pymongo
-import yaml
-import sys
-import os
-import datetime
-import jwt
-from flask_cors import CORS, cross_origin
+from login.login_service import _build_cors_preflight_response, authenticate, is_authorized
+from flask_cors import CORS
 
 app = Flask(__name__)
 cors = CORS(app)
